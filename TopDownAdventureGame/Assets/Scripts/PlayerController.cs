a72f490244125ee7a93e9b4595e5a88653809914
Gexes <145797289+Gexes@users.noreply.github.com>
2023-10-06 11:12:07 -0400

    Game Changes
    Added - colored circles
    Added - Character

---
+++ TopDownAdventureGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //importing SceneManagement library
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            //player moves up
+            newPosition.y += speed;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            //player moves down
+            newPosition.y -= speed;
+        }
+
+        //player moves left
+        if (Input.GetKey("a"))
+        {
+            newPosition.x -= speed;
+        }
+
+        //player moves right
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+        }
+
+        transform.position = newPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("door"))
+        {
+            Debug.Log("hit");
+            SceneManager.LoadScene(1); //access SceneManager class for LoadScene function
+        }
+    }
+}
